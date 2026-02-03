858aa4e9c1ed7204cf57f4b6e6a4a49783ecc429
C. Dylan Shearer <dylan@nekonya.info>
2026-02-02 21:14:33 -0500

    Add CI workflow for PR builds (#1)

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(tseslint.configs.recommended, {
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_" },
+    ],
+  },
+});
